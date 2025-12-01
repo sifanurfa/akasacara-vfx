39fa81e3e3dd68310b80fa5348d9858c79f4f927
sifanurfa <assyka09@gmail.com>
2025-12-01 18:45:12 +0700

    feat: fetch vfx api

--- src/app/vfx/VFXShowcaseSection.tsx
+++ src/app/vfx/VFXShowcaseSection.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from 'next/image'
 import styles from "./Poster.module.css";
+import { VFXApi } from "@/lib/api";
+import { VFX } from "@/types/api/types";
+import Link from "next/link";
 
 export default function ShowcaseSection() {
+    const [VFX, setVFX] = useState<VFX[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+            const data = await VFXApi.getAll({ limit: 3 });
+            setVFX(data);
+            } catch (err) {
+            console.error("Failed to fetch works:", err);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="flex flex-col py-section px-container items-start gap-xl self-stretch">
             <div className="flex flex-col px-4xl items-center gap-6 self-stretch">
@@ -11,71 +30,58 @@ export default function ShowcaseSection() {
             </div>
             <div className="flex flex-col  items-start self-stretch">
                 <div className="flex flex-col py-3xl items-start gap-2xl self-stretch">
-                    <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-l self-stretch">
-                         <div className={`${styles.pictPorto} flex flex-1`}>
-                            <Image
-                                src="/assets/darah_nyai_2.png"
-                                alt="Portfolio Image"
-                                fill
-                                className="object-cover"
-                            />
+                    {VFX.map((item, idx) => (                        
+                        <div key={item.id} className="grid grid-cols-1 lg:grid-cols-2 items-center gap-l self-stretch">
+                            {idx % 2 === 1 ? (
+                                <>
+                                <div className="flex flex-col justify-center items-start lg:gap-4xl gap-l flex-1">
+                                    <div className="flex flex-col items-start gap-s self-stretch">
+                                        <span className="headline-2 vfx-text-title self-stretch">{item.title}</span>
+                                        <span className="sub-heading-reg vfx-text-subtitle-1 self-stretch line-clamp-4">{item.description}</span>
+                                    </div>
+                                    <div onClick={() => window.open(item.link, "_blank", "noopener,noreferrer")} className="flex items-center gap-5 cursor-pointer">
+                                        <span className="button-secondary vfx-text-title">WATCH HERE</span>
+                                        <span className="watch-here vfx-text-title">-&gt;</span>
+                                    </div>
+                                </div>
+                                <div className={`${styles.pictPorto} flex flex-1`}>
+                                    <Image
+                                        src={item.image}
+                                        alt={item.title}
+                                        fill
+                                        className="object-cover"
+                                    />
+                                </div>
+                                </>
+                            ) : (
+                                <>
+                                <div className={`${styles.pictPorto} flex flex-1`}>
+                                    <Image
+                                        src={item.image}
+                                        alt={item.title}
+                                        fill
+                                        className="object-cover"
+                                    />
+                                </div>
+                                <div className="flex flex-col justify-center items-start lg:gap-4xl gap-l flex-1">
+                                    <div className="flex flex-col items-start gap-s self-stretch">
+                                        <span className="headline-2 vfx-text-title self-stretch">{item.title}</span>
+                                        <span className="sub-heading-reg vfx-text-subtitle-1 self-stretch line-clamp-4">{item.description}</span>
+                                    </div>
+                                    <div onClick={() => window.open(item.link, "_blank", "noopener,noreferrer")} className="flex items-center gap-5 cursor-pointer">
+                                        <span className="button-secondary vfx-text-title">WATCH HERE</span>
+                                        <span className="watch-here vfx-text-title">-&gt;</span>
+                                    </div>
+                                </div>
+                                </>
+                            )}
                         </div>
-                        <div className="flex flex-col justify-center items-start lg:gap-4xl gap-l flex-1">
-                            <div className="flex flex-col items-start gap-s self-stretch">
-                                <span className="headline-2 vfx-text-title self-stretch">Darah Nyai</span>
-                                <span className="sub-heading-reg vfx-text-subtitle-1 self-stretch">Dive behind the scenes of Darah Nyai</span>
-                            </div>
-                            <div className="flex items-center gap-5">
-                                <span className="button-secondary vfx-text-title">WATCH HERE</span>
-                                <span className="watch-here vfx-text-title">-&gt;</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-l self-stretch">
-                        <div className="flex flex-col justify-center items-start lg:gap-4xl gap-l flex-1 order-2 lg:order-1">
-                            <div className="flex flex-col items-start gap-s self-stretch">
-                                <span className="headline-2 vfx-text-title self-stretch">Gowok</span>
-                                <span className="sub-heading-reg vfx-text-subtitle-1 self-stretch">Dive behind the scenes of Gowok</span>
-                            </div>
-                            <div className="flex items-center gap-5">
-                                <span className="button-secondary vfx-text-title">WATCH HERE</span>
-                                <span className="watch-here vfx-text-title">-&gt;</span>
-                            </div>
-                        </div>
-                        <div className={`${styles.pictPorto} flex flex-1 order-1 lg:order-2`}>
-                            <Image
-                                src="/assets/gowok2.png"
-                                alt="Portfolio Image"
-                                fill
-                                className="object-cover"
-                            />
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-l self-stretch">
-                        <div className={`${styles.pictPorto} flex flex-1`}>
-                            <Image
-                                src="/assets/serigala_langit.png"
-                                alt="Portfolio Image"
-                                fill
-                                className="object-cover"
-                            />
-                        </div>
-                        <div className="flex flex-col justify-center items-start lg:gap-4xl gap-l flex-1">
-                            <div className="flex flex-col items-start gap-s self-stretch">
-                                <span className="headline-2 vfx-text-title self-stretch">Serigala Langit</span>
-                                <span className="sub-heading-reg vfx-text-subtitle-1 self-stretch">Dive behind the scenes of Serigala Langit</span>
-                            </div>
-                            <div className="flex items-center gap-5">
-                                <span className="button-secondary vfx-text-title">WATCH HERE</span>
-                                <span className="watch-here vfx-text-title">-&gt;</span>
-                            </div>
-                        </div>
-                    </div>
+                    ))}                    
                 </div>
-                <div className="flex justify-center items-center gap-m self-stretch">
+                <Link href="/vfx/showcase" className="flex justify-center items-center gap-m self-stretch cursor-pointer">
                     <span className="button-main text-center vfx-text-title">SEE ALL</span>
                     <span className="see-all vfx-text-title">&gt;</span>
-                </div>
+                </Link>
             </div>
         </div>
     )
